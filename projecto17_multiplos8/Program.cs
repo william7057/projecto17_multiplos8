1220ff0fc13e6ae5c2525febd746c6505cba71b2
william <factzure@hotmail.com>
2021-09-10 12:37:18 -0500

    Agregar archivos de proyecto.

---
+++ projecto17_multiplos8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecto17_multiplos8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x<=500)
+            {
+                if (x%8==0)
+                {
+                    Console.WriteLine(x); // %
+                }
+                x = x + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
